85b6b634a170f44c8c374393c709a5846b889e8b
hitesh kumra <hiteshkumra4@gmail.com>
2024-01-10 19:50:25 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 1.0) {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
